1ed6d83c6f36f7e6a459c86d07972f443fcaa668
Zhang Dingyuan <zhangdingyuan@deepin.org>
2024-05-14 13:47:18 +0800

    chore: remove treeland socket helper
    treeland will use systemd socket.
    
    Log:

--- src/daemon/SingleWaylandDisplayServer.h
+++ src/daemon/SingleWaylandDisplayServer.h
@@ -24,15 +24,11 @@ Q_SIGNALS:
     void createWaylandSocketFinished();
 
 public Q_SLOTS:
-    void createWaylandSocket(const QString &user);
-    void deleteWaylandSocket(const QString &user);
     void activateUser(const QString &user);
     QString getUserWaylandSocket(const QString &user) const;
 
 private:
     DDM::SocketServer *m_socketServer;
-    QLocalServer *m_helperServer;
-    QLocalSocket *m_helperSocket;
     QList<QLocalSocket*> m_greeterSockets;
     QMap<QString, QString> m_waylandSockets;
     QProcess *m_helper;
