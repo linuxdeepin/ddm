3f55fc9ca0f289a9b88bff65480fd720f6020e11
April & May & June <us@calsys.org>
2026-02-25 19:18:43 +0800

    feat: Upgrade treeland_ddm protocol to version 2
    In this upgrade we rewrite all treeland-ddm interactions, includes those
    originally in SocketServer / GreeterProxy, into wayland communication.
    There's only one true treeland-ddm interaction which should on behalf of
    the Wayland protocol.

--- src/daemon/TreelandConnector.h
+++ src/daemon/TreelandConnector.h
@@ -2,34 +2,65 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include <QObject>
-#include <QSocketNotifier>
+
+class QSocketNotifier;
+class QTimer;
 
 struct wl_display;
 struct wl_callback;
-struct treeland_ddm_v1;
+struct treeland_ddm_v2;
 
 namespace DDM {
-class TreelandConnector : QObject {
-    Q_OBJECT
-public:
-    TreelandConnector();
-    ~TreelandConnector();
-    bool isConnected();
-    void setPrivateObject(struct treeland_ddm_v1 *ddm);
-    void setSignalHandler();
-    void connect(const QString socketPath);
-    void disconnect();
+    class Display;
 
-    void switchToGreeter();
-    void switchToUser(const QString username);
-    void ackVtSwitch(const int vtnr);
-    void activateSession();
-    void deactivateSession();
-    void enableRender();
-    struct wl_callback *disableRender();
-private:
-    struct wl_display *m_display { nullptr };
-    QSocketNotifier *m_notifier { nullptr };
-    struct treeland_ddm_v1 *m_ddm { nullptr };
-};
-}
+    class TreelandConnector : public QObject {
+        Q_OBJECT
+    public:
+        TreelandConnector(Display *display);
+        ~TreelandConnector();
+
+        /**
+         * @brief Connect to treeland.
+         *
+         * Continuously tries until success.
+         *
+         * This should be called by Display when starting.
+         */
+        void connect();
+
+        /**
+         * @brief Disconnect from treeland.
+         *
+         * This should be called by Display when stopping.
+         */
+        void disconnect();
+
+        ///////////////////////////////////////////////////////////
+        // Request wrappers                                      //
+        // Call them once you want to send something to treeland //
+        // Documentations are available in treeland-ddm-v2.xml   //
+        ///////////////////////////////////////////////////////////
+
+        void capabilities(uint32_t capabilities) const;
+        void userLoggedIn(const QString &username, const QString &session) const;
+        void authenticationFailed(uint32_t error) const;
+        void switchToGreeter() const;
+        void switchToUser(const QString &username) const;
+        void activateSession() const;
+        void deactivateSession() const;
+        void enableRender() const;
+        struct wl_callback *disableRender() const;
+
+        /** The proxy object for sending requests. Mainly for private use. */
+        struct treeland_ddm_v2 *proxy{ nullptr };
+
+    private Q_SLOTS:
+        void tryConnect();
+        void connected();
+
+    private:
+        struct wl_display *m_display{ nullptr };
+        QSocketNotifier *m_notifier{ nullptr };
+        QTimer *m_connectTimer{ nullptr };
+    };
+} // namespace DDM
