LFRon/ddm-fix

fix(daemon): handle Treeland VT activation through dde-seatd
Route Treeland-owned VT events back to the greeter or user session.

将Treeland管理的VT事件路由回greeter或用户会话。

Log: 完善Treeland VT激活处理

Influence: DDM根据dde-seatd的VT激活事件恢复Treeland渲染和greeter状态，便于定位tty切换问题。

src/daemon/TreelandDisplayServer.cpp
@@ -27,9 +27,13 @@ TreelandDisplayServer::TreelandDisplayServer(SocketServer *socketServer, Display
     , m_socketServer(socketServer) {
     connect(m_socketServer, &SocketServer::connected, this, [this, parent](QLocalSocket *socket) {
         m_greeterSockets << socket;
+        qWarning("Treeland greeter socket connected: socket=%p total=%lld",
+                 socket, static_cast<long long>(m_greeterSockets.size()));
     });
     connect(m_socketServer, &SocketServer::disconnected, this, [this](QLocalSocket *socket) {
         m_greeterSockets.removeOne(socket);
+        qWarning("Treeland greeter socket disconnected: socket=%p total=%lld",
+                 socket, static_cast<long long>(m_greeterSockets.size()));
     });
 }
 
@@ -72,11 +76,16 @@ void TreelandDisplayServer::stop() {
 }
 
 void TreelandDisplayServer::activateUser(const QString &user, int xdgSessionId) {
+    qWarning("Send greeter activation: user=%s xdgSessionId=%d sockets=%lld",
+             qPrintable(user), xdgSessionId, static_cast<long long>(m_greeterSockets.size()));
     for (auto greeter : m_greeterSockets) {
         if (user == "dde") {
+            qWarning("Sending SwitchToGreeter to socket=%p", greeter);
             SocketWriter(greeter) << quint32(DaemonMessages::SwitchToGreeter);
         }
 
+        qWarning("Sending UserActivateMessage to socket=%p user=%s xdgSessionId=%d",
+                 greeter, qPrintable(user), xdgSessionId);
         SocketWriter(greeter) << quint32(DaemonMessages::UserActivateMessage) << user << xdgSessionId;
     }
 }

linuxdeepin/ddm

src/daemon/TreelandDisplayServer.cpp
@@ -27,9 +27,13 @@ TreelandDisplayServer::TreelandDisplayServer(SocketServer *socketServer, Display
     , m_socketServer(socketServer) {
     connect(m_socketServer, &SocketServer::connected, this, [this, parent](QLocalSocket *socket) {
         m_greeterSockets << socket;
+        qWarning("Treeland greeter socket connected: socket=%p total=%lld",
+                 socket, static_cast<long long>(m_greeterSockets.size()));
     });
     connect(m_socketServer, &SocketServer::disconnected, this, [this](QLocalSocket *socket) {
         m_greeterSockets.removeOne(socket);
+        qWarning("Treeland greeter socket disconnected: socket=%p total=%lld",
+                 socket, static_cast<long long>(m_greeterSockets.size()));
     });
 }
 
@@ -72,11 +76,16 @@ void TreelandDisplayServer::stop() {
 }
 
 void TreelandDisplayServer::activateUser(const QString &user, int xdgSessionId) {
+    qDebug("Send greeter activation: user=%s xdgSessionId=%d sockets=%lld",
+           qPrintable(user), xdgSessionId, static_cast<long long>(m_greeterSockets.size()));
     for (auto greeter : m_greeterSockets) {
         if (user == "dde") {
+            qDebug("Sending SwitchToGreeter to socket=%p", greeter);
             SocketWriter(greeter) << quint32(DaemonMessages::SwitchToGreeter);
         }
 
+        qDebug("Sending UserActivateMessage to socket=%p user=%s xdgSessionId=%d",
+               greeter, qPrintable(user), xdgSessionId);
         SocketWriter(greeter) << quint32(DaemonMessages::UserActivateMessage) << user << xdgSessionId;
     }
 }